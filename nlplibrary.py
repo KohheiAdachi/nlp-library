95962554811c52c360c56be5123390e1f87ca86b
Kohei Adachi <d13301@yonago.kosen-ac.jp>
2019-04-29 13:22:58 +0900

    added nlplibrary

---
+++ nlplibrary.py
@@ -0,0 +1,50 @@
+# -*- coding:utf-8 -*-
+
+import MeCab
+
+
+def tokenize(doc):
+    """形態素解析する
+    Parameters
+    -------
+    doc:str
+        文字列
+    Returns
+    -----
+    words:list
+        形態素解析後のリスト
+    """
+    mecab = MeCab.Tagger("-Ochasen")
+    lines = mecab.parse(doc).splitlines()
+    words = []
+    stop_words = []
+    for line in lines:
+        chunks = line.split('\t')
+        if len(chunks) > 3 and not chunks[2] in stop_words:
+            if chunks[3].startswith('動詞'):
+                words.append(chunks[2])
+            if chunks[3].startswith('名詞') and not chunks[0] in stop_words:
+                words.append(chunks[0])
+            if chunks[3].startswith('副詞'):
+                words.append(chunks[0])
+            if chunks[3].startswith('感動詞'):
+                words.append(chunks[0])
+            if chunks[3].startswith('形容詞'):
+                words.append(chunks[2])
+            if chunks[3].startswith('形容動詞'):
+                words.append(chunks[2])    
+    return words
+
+def tokenize_all(doc):
+    mecab = MeCab.Tagger("-Ochasen")
+    lines = mecab.parse(doc).splitlines()
+    words = []
+    stop_words = []
+    for line in lines:
+        chunks = line.split('\t')
+        if not chunks[0] in stop_words:
+            words.append(chunks[0])
+    
+    return words
+           
+        
